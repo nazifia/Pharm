9ddb0029148c6e16f2d8ca783c086e1e1b82b16c
nazifia <143083164+nazifia@users.noreply.github.com>
2025-04-07 06:41:34 +0000

    receipt modification

--- pharmapp/templatetags/custom_filters.py
+++ pharmapp/templatetags/custom_filters.py
@@ -2,6 +2,14 @@ from django import template
 
 register = template.Library()
 
+@register.filter
+def multiply(value, arg):
+    """Multiplies the value by the argument"""
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return 0
+
 @register.filter(name='test_filter')
 def test_filter(value):
     return str(value)
@@ -11,5 +19,6 @@ def calculate_period_total(histories):
     if not histories:
         return 0
     return sum(history.subtotal for history in histories)
+
 
 
