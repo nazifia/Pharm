2c31a91f85498a3c27f85533cf2565694c05a836
nazifia <143083164+nazifia@users.noreply.github.com>
2025-07-24 07:54:47 +0000

    correction

--- pharmapp/chat/views.py
+++ pharmapp/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
+from django.contrib import messages
 from django.db.models import Q, Count, Max
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
